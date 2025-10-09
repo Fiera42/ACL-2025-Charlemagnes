e79941e38cc253a7291c73e42e322c4f42d22cdb
Fiera <46507472+Fiera42@users.noreply.github.com>
2025-10-09 17:13:28 +0200

    Fixed reccurent appointement

--- src/domain/entities/ReccurentAppointment.ts
+++ src/domain/entities/ReccurentAppointment.ts
@@ -2,17 +2,31 @@ import { Appointment } from "./Appointment";
 import { RecursionRule } from "./RecursionRule";
 
 export class RecurrentAppointment extends Appointment {
-    constructor(
+  constructor(
     public id: string | null,
     public calendarId: string,
     public title: string,
     public description: string,
     public startDate: Date,
     public endDate: Date,
-    public createdAt: Date = new Date(),
+    public ownerId: string,
     public recursionRule: RecursionRule = RecursionRule.WEEKLY,
+    public createdAt: Date = new Date(),
+    public updatedAt: Date = new Date(),
+    public updatedBy: string | null = null
   ) {
-    super(id, calendarId, title, description, startDate, endDate, createdAt)
+    super(
+      id,
+      calendarId,
+      title,
+      description,
+      startDate,
+      endDate,
+      ownerId,
+      createdAt,
+      updatedAt,
+      updatedBy
+    );
   }
 
   static createRecurrent(
@@ -21,8 +35,28 @@ export class RecurrentAppointment extends Appointment {
     description: string,
     startDate: Date,
     endDate: Date,
+    ownerId: string,
     recursionRule: RecursionRule
   ): RecurrentAppointment {
-    return new RecurrentAppointment(null, calendarId, title, description, startDate, endDate, undefined, recursionRule);
+    return new RecurrentAppointment(
+      null,
+      calendarId,
+      title,
+      description,
+      startDate,
+      endDate,
+      ownerId,
+      recursionRule
+    );
+  }
+
+  isValid(): boolean {
+    return Boolean(
+      this.calendarId &&
+        this.title &&
+        this.startDate &&
+        this.ownerId &&
+        this.recursionRule
+    );
   }
 }
