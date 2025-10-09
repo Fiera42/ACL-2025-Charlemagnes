cbe00f23b350c0cf7701be04c7e628d6c66bf9cb
Fiera <46507472+Fiera42@users.noreply.github.com>
2025-10-09 16:44:29 +0200

    Non-implemented service classes

---
+++ src/application/services/AppointmentService.ts
@@ -0,0 +1,96 @@
+import { Appointment } from "../../domain/entities/Appointment";
+import { CalendarServiceResponse } from "../../domain/entities/CalendarServiceResponse";
+import { RecurrentAppointment } from "../../domain/entities/ReccurentAppointment";
+import { RecursionRule } from "../../domain/entities/RecursionRule";
+import { IAppointmentService } from "../../domain/interfaces/IAppointmentService";
+import { ICalendarDB } from "../../domain/interfaces/ICalendarDB";
+
+export class AppointmentService implements IAppointmentService {
+  calendarDB: ICalendarDB;
+
+  constructor(calendarDB: ICalendarDB) {
+    this.calendarDB = calendarDB;
+  }
+
+  createAppointment(
+    ownerId: string,
+    calendarId: string,
+    title: string,
+    description: string,
+    startDate: Date,
+    endDate: Date
+  ): Promise<Appointment> {
+    throw new Error("Method not implemented.");
+  }
+
+  createRecurrentAppointment(
+    ownerId: string,
+    calendarId: string,
+    title: string,
+    description: string,
+    startDate: Date,
+    endDate: Date,
+    recursionRule: RecursionRule
+  ): Promise<RecurrentAppointment> {
+    throw new Error("Method not implemented.");
+  }
+
+  deleteAppointment(
+    ownerId: string,
+    appointmentId: string
+  ): Promise<CalendarServiceResponse> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateAppointment(
+    ownerId: string,
+    appointmentId: string,
+    appointment: Partial<Appointment>
+  ): Promise<CalendarServiceResponse> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateRecurrentAppointment(
+    ownerId: string,
+    appointmentId: string,
+    appointment: Partial<RecurrentAppointment>
+  ): Promise<CalendarServiceResponse> {
+    throw new Error("Method not implemented.");
+  }
+
+  shareAppointment(
+    ownerId: string,
+    appointmentId: string,
+    sharedToId: string
+  ): Promise<CalendarServiceResponse> {
+    throw new Error("Method not implemented.");
+  }
+
+  unShareAppointment(
+    ownerId: string,
+    appointmentId: string,
+    sharedToId: string
+  ): Promise<CalendarServiceResponse> {
+    throw new Error("Method not implemented.");
+  }
+
+  getAppointmentById(id: string): Promise<Appointment | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  getAppointmentsByCalendarId(calendarId: string): Promise<Appointment[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  getConflictsOfUser(
+    ownerId: string
+  ): Promise<{ appointmentA: Appointment; appointmentB: Appointment }[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  getConflictsOfCalendar(
+    calendarId: string
+  ): Promise<{ appointmentA: Appointment; appointmentB: Appointment }[]> {
+    throw new Error("Method not implemented.");
+  }
+}
---
+++ src/application/services/CalendarService.ts
@@ -0,0 +1,56 @@
+import { Calendar } from "../../domain/entities/Calendar";
+import { CalendarServiceResponse } from "../../domain/entities/CalendarServiceResponse";
+import { ICalendarDB } from "../../domain/interfaces/ICalendarDB";
+import { ICalendarService } from "../../domain/interfaces/ICalendarService";
+
+export class CalendarService implements ICalendarService {
+  calendarDB: ICalendarDB;
+
+  constructor(calendarDB: ICalendarDB) {
+    this.calendarDB = calendarDB;
+  }
+
+  createCalendar(
+    ownerId: string,
+    name: string,
+    description: string
+  ): Promise<Calendar | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  removeCalendar(ownerId: string, calendarId: string): Promise<CalendarServiceResponse> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateCalendar(
+    ownerId: string,
+    calendarId: string,
+    calendar: Partial<Calendar>
+  ): Promise<CalendarServiceResponse> {
+    throw new Error("Method not implemented.");
+  }
+
+  getCalendarById(id: string): Promise<Calendar | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  getCalendarsByOwnerId(ownerId: string): Promise<Calendar[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  shareCalendar(
+    ownerId: string,
+    calendarId: string,
+    sharedToId: string
+  ): Promise<CalendarServiceResponse> {
+    throw new Error("Method not implemented.");
+  }
+
+  unShareCalendar(
+    ownerId: string,
+    calendarId: string,
+    sharedToId: string
+  ): Promise<CalendarServiceResponse> {
+    throw new Error("Method not implemented.");
+  }
+}
