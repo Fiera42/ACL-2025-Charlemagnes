846d7794a0c27c1dbad16f51747156b820211ed8
KerrianG <kerrian.gesnel2@etu.univ-lorraine.fr>
2025-10-25 00:15:51 +0200

    fichier config

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [vue()],
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            },
+            '/test-db': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            },
+            '/test-insert': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            },
+            '/test-clean': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            },
+        },
+    },
+});
