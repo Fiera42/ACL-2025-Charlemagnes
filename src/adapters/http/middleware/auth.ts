3047c1fea5cf3f1fd2ef02034111e9afb5d21d9b
Fiera <46507472+Fiera42@users.noreply.github.com>
2025-11-03 18:18:05 +0100

    Auth routes ??

--- src/adapters/http/middleware/auth.ts
+++ src/adapters/http/middleware/auth.ts
@@ -1,5 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import {webcrypto} from "node:crypto";
+
+// Add random noise to the token to invalidate everything on server re-start
+const JWT_RANDOM_INSTANCE: string = webcrypto.getRandomValues(new Uint8Array(10)).join();
+const JWT_SECRET: string = (process.env.JWT_SECRET || 'secret_par_defaut') + JWT_RANDOM_INSTANCE;
+
+const JWT_TOKEN_DURATION_SECONDS: number = 1800 // IN SECONDS
 
 export interface JwtPayload {
   userId: string;
@@ -23,9 +30,15 @@ export function authenticateToken(
     return;
   }
 
+  updateBannedTokens();
+  if(bannedTokens.has(token)) {
+      res.status(403).json({ error: 'Token invalide ou expiré' });
+      return;
+  }
+
   jwt.verify(
       token,
-      process.env.JWT_SECRET || 'secret_par_defaut',
+      JWT_SECRET,
       (err, user) => {
         if (err) {
           res.status(403).json({ error: 'Token invalide ou expiré' });
@@ -41,10 +54,28 @@ export function createAuthToken(user: JwtPayload): Promise<string> {
   return new Promise((resolve, _) => {
     jwt.sign(
         user,
-        process.env.JWT_SECRET || 'secret_par_defaut',
+        JWT_SECRET,
+        {expiresIn: JWT_TOKEN_DURATION_SECONDS * 10},
         (err: any, asyncToken: any) => {
           if (err) throw err;
           resolve(asyncToken);
         });
   });
+}
+
+const bannedTokens: Map<string, number> = new Map();
+
+export function banAuthToken(token: string): void {
+    updateBannedTokens();
+
+    // Ban the new token
+    const validityTime: number = Date.now() + (JWT_TOKEN_DURATION_SECONDS * 10);
+    bannedTokens.set(token, validityTime);
+}
+
+export function updateBannedTokens() {
+    const now = Date.now();
+    for (const [token, date] of bannedTokens) {
+        if (date <= now) bannedTokens.delete(token);
+    }
 }
--- src/adapters/http/routes/auth.ts
+++ src/adapters/http/routes/auth.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response, Router } from 'express';
+import express, {Request, Response, Router} from 'express';
 import {ServiceFactory} from "../../factories/ServiceFactory.ts";
-import {createAuthToken} from "../middleware/auth.ts";
+import {AuthenticatedRequest, authenticateToken, banAuthToken, createAuthToken} from "../middleware/auth.ts";
+import {ServiceResponse} from "../../../domain/entities/ServiceResponse.ts";
 
 const router: Router = express.Router();
 const authService = ServiceFactory.getAuthService()
@@ -11,19 +12,89 @@ router.get('/', (req: Request, res: Response) => {
 });
     */
 
+router.get('/user/:email', async (req: Request, res: Response) => {
+    try {
+        const user = await authService.findUserByEmail(req.params.email);
+        if (user === null) {
+            return res.status(404).json({ error: 'Utilisateur inconnu' });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: "Erreur lors de la récupération de l'utilisateur" });
+    }
+});
+
+router.put('/user/:id', authenticateToken, async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const response = (req.params.id === req.user!.userId)
+            ? await authService.updateUser(req.user!.userId, req.body)
+            : ServiceResponse.FORBIDDEN;
+
+        switch (response) {
+            case ServiceResponse.SUCCESS:
+                res.status(200);
+                break;
+            case ServiceResponse.FAILED:
+                res.status(500).json({ error: "Erreur lors de la mise à jour de l'utilisateur" });
+                break;
+            case ServiceResponse.RESOURCE_NOT_EXIST:
+                res.status(404).json({ error: 'Utilisateur inconnu' });
+                break;
+            case ServiceResponse.FORBIDDEN:
+                res.status(403);
+                break;
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la mise à jour du calendrier' });
+    }
+});
+
+router.delete('/user/:id', authenticateToken, async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const response = (req.params.id === req.user!.userId)
+            ? await authService.deleteUser(req.user!.userId)
+            : ServiceResponse.FORBIDDEN;
+
+        switch (response) {
+            case ServiceResponse.SUCCESS:
+                res.status(200);
+
+                // User deletion completed, the token should no longer be used for logging in
+                const authHeader = req.headers['authorization'];
+                const token = authHeader?.split(' ')[1];
+
+                if (token) {
+                    banAuthToken(token);
+                }
+                break;
+            case ServiceResponse.FAILED:
+                res.status(500).json({ error: 'Erreur lors de la suppression du calendrier' });
+                break;
+            case ServiceResponse.RESOURCE_NOT_EXIST:
+                res.status(404).json({ error: 'Utilisateur inconnu' });
+                break;
+            case ServiceResponse.FORBIDDEN:
+                res.status(403);
+                break;
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la suppression du calendrier' });
+    }
+});
+
 router.post('/login', async (req: Request, res: Response) => {
     try {
         const { id, password } = req.body;
         const user = await authService.findUserById(id);
         if(user !== null && await authService.verifyPassword(user, password)) {
             const token = createAuthToken({userId: user.id as string, email: user.email})
-            res.status(200).json(token);
+            res.status(205).json(token);
         }
         else if(user === null) {
-            res.status(404);
+            res.status(404).json({ error: 'Utilisateur inconnu' });
         }
         else {
-            res.status(401);
+            res.status(401).json({ error: 'Mot de passe incorrect' });
         }
 
     } catch (error) {
@@ -42,9 +113,28 @@ router.post('/register', async (req: Request, res: Response) => {
     }
 });
 
-router.get('/logout', async (req: Request, res: Response) => {
-    // Logique de déconnexion
-    res.json({ message: 'Logout endpoint' });
+router.delete('/logout/', authenticateToken, async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const { id } = req.body;
+        if(id === req.user!.userId) {
+            res.status(403);
+            return;
+        }
+
+        const authHeader = req.headers['authorization'];
+        const token = authHeader?.split(' ')[1];
+
+        if (!token) {
+            res.status(401).json({ error: 'Token manquant' });
+            return;
+        }
+
+        banAuthToken(token);
+
+        res.status(205).json(token);
+    } catch (error) {
+        res.status(500).json({ error: "Erreur lors de la déconnexion" });
+    }
 });
 
 export default router;
