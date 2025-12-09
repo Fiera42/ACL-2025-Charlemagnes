54e40cf84449b24e699da358bcd2e661c4bbbc4c
Alex Collin <93080448+SkyPurl@users.noreply.github.com>
2025-12-09 22:32:32 +0100

    Update AppHeader.vue

--- src/client/components/layout/AppHeader.vue
+++ src/client/components/layout/AppHeader.vue
@@ -17,12 +17,12 @@
           <div class="flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none">
               <path
-                d="M8 2V5M16 2V5M3. 5 9. 09H20.5M21 8. 5V17C21 20 19.5 22 16 22H8C4.5 22 3 20 3 17V8.5C3 5.5 4.5 3.5 8 3.5H16C19.5 3.5 21 5.5 21 8.5Z"
-                stroke="#4F46E5" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"
-                stroke-linejoin="round" />
+                  d="M8 2V5M16 2V5M3.5 9.09H20.5M21 8.5V17C21 20 19.5 22 16 22H8C4.5 22 3 20 3 17V8.5C3 5.5 4.5 3.5 8 3.5H16C19.5 3.5 21 5.5 21 8.5Z"
+                  stroke="#4F46E5" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"
+                  stroke-linejoin="round"/>
               <path
-                d="M15. 6947 13.7H15.7037M15.6947 16.7H15.7037M11.9955 13.7H12.0045M11.9955 16.7H12.0045M8. 29431 13.7H8.30329M8.29431 16.7H8.30329"
-                stroke="#4F46E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  d="M15.6947 13.7H15.7037M15.6947 16.7H15.7037M11.9955 13.7H12.0045M11.9955 16.7H12.0045M8.29431 13.7H8.30329M8.29431 16.7H8.30329"
+                  stroke="#4F46E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
             <h1 class="text-xl font-bold text-gray-900">Charl'endar</h1>
           </div>
